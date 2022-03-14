fdecc43fda2722af1bdbe549d618e800cf3c50f0
JingyaLiu123 <88065941+JingyaLiu123@users.noreply.github.com>
2022-03-14 22:02:46 +1030

    Update Computer.cpp

--- Computer.cpp
+++ Computer.cpp
@@ -1,15 +1,11 @@
 #include "Computer.h"
 #include <iostream>
-using namespace std;
 
 Computer::Computer()
 {
     Move = 'R';
 }
-/*char Computer::makeMove(char M){
-    Move = M;
-    M = 'R';
-}*/
+
 char Computer::getMove(){
     return Move;
 }
