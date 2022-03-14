9410979df43070faeae4e287c811c5bbee8abfec
JingyaLiu123 <88065941+JingyaLiu123@users.noreply.github.com>
2022-03-14 16:51:35 +1030

    Update Human.h

---
+++ Human.h
@@ -0,0 +1,18 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+#include <iostream>
+using namespace std;
+
+//initate class
+class Human
+{
+    public:
+        Human();
+        char makeMove();
+        char getMove();
+
+    private:
+        char hMove;
+};
+
+#endif
