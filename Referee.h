633e84df9953fcd332ea48753bb7b7e179055213
JingyaLiu123 <88065941+JingyaLiu123@users.noreply.github.com>
2022-03-14 21:36:11 +1030

    Update Referee.h

--- Referee.h
+++ Referee.h
@@ -10,7 +10,7 @@ class Referee
 {
     public:
         Referee();
-        char refGame(Human player1, Computer player2);
+        char refGame(Human playerH, Computer playerC);
     private:
         char Result;
         char Human_move;
