dc8c301c9fe82aadc578c746c41e73b2cf213a2f
JingyaLiu123 <88065941+JingyaLiu123@users.noreply.github.com>
2022-03-14 16:51:57 +1030

    Update Referee.cpp

---
+++ Referee.cpp
@@ -0,0 +1,25 @@
+#include "Referee.h"
+#include "Computer.h"
+#include "Human.h"
+#include <iostream>
+using namespace std;
+
+Referee::Referee()
+{
+    Result = 'a';
+    //cout << Result << endl;
+}
+
+char Referee::refGame(Human player1, Computer player2)
+{
+    Human_move = player1.makeMove();
+    Computer_move = player2.getMove();
+    if (Human_move == Computer_move){
+        Result = 'T';
+    } else if (Human_move == 'P'){
+        Result == 'W';
+    } else if (Human_move == 'S'){
+        Result == 'L';
+    }
+    return Result;
+}
