ebe46259f476e2215d6ca1daab7651aa46540b8d
JingyaLiu123 <88065941+JingyaLiu123@users.noreply.github.com>
2022-03-14 16:52:14 +1030

    Update Referee.h

---
+++ Referee.h
@@ -0,0 +1,19 @@
+#ifndef REFEREE_H
+#define REFEREE_H
+#include <iostream>
+#include <stdio.h>
+#include "Human.h"
+#include "Computer.h"
+using namespace std;
+
+class Referee
+{
+    public:
+        Referee();
+        char refGame(Human player1, Computer player2);
+    private:
+        char Result;
+        char Human_move;
+        char Computer_move;
+};
+#endif
