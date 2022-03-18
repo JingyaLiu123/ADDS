4af537e39ba7054f91da1597db6e2aab5831db88
Jingya Liu <88065941+JingyaLiu123@users.noreply.github.com>
2022-03-18 13:27:12 +1030

    Add files via upload

---
+++ Avalanche.cpp
@@ -0,0 +1,5 @@
+#include "Avalanche.h"
+#include <iostream>
+
+Avalanche::Avalanche()
+{}
---
+++ Avalanche.h
@@ -0,0 +1,19 @@
+#ifndef AVALANCHE_H
+#define AVALANCHE_H
+#include "Computer.h"
+
+class Avalanche: public Computer
+{
+    public:
+        Avalanche();
+    /*public:
+        Avalanche();
+        char getMove();
+        ~Avalanche();
+    private:
+        char AvMove;*/
+};
+
+
+
+#endif 
