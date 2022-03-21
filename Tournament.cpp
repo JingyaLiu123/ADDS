4f5e03747009626b4267bfbb0a79b222b781267a
Jingya Liu <88065941+JingyaLiu123@users.noreply.github.com>
2022-03-21 21:40:35 +1030

    Add files via upload

--- Tournament.cpp
+++ Tournament.cpp
@@ -1,1 +1,14 @@
-#include "Toolbox.h"
+#include "Tournament.h"
+#include "Player.h"
+#include <vector>
+#include <array>
+using namespace std;
+
+
+Tournament::Tournament(){}
+
+Player * Tournament::run(array<Player *, 8>  competitors){}
+
+
+Tournament::~Tournament(){}
+
--- Tournament.h
+++ Tournament.h
@@ -1,4 +1,15 @@
 #ifndef TOURNAMENT_H
 #define TOURNAMENT_H
+#include <vector>
+#include <array>
+
+class Tournament
+{
+    public:
+        Tournament();
+        Player * run(array<Player *, 8>  competitors);
+        ~Tournament();
+
+};
 
 #endif
