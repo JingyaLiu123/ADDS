1941dbf2587e1c6f4876b510530e69160ba4f34a
Jingya Liu <88065941+JingyaLiu123@users.noreply.github.com>
2022-04-26 17:38:39 +0930

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,2 @@
-#include <iostream>
-#include "Reverse.h"
-#include "TruckLoads.h"
-#include "EfficientTruckLoads.h"
-using namespace std;
-
-int main(void) 
-{
-    Reverse Sobj;
-    cout << Sobj.reverseString("Andy") << endl;  
-    Reverse Dobj;
-    cout << Dobj.reverseDigit(-12345) << endl;  
-    Truckloads Tobj;
-    cout << Tobj.numTrucks(-1, 1) << endl;
-    EfficientTruckloads ETobj;
-    cout << ETobj.numEffTrucks(2, 1) << endl;
-}
+#include <iostream>>
+#include <string>
