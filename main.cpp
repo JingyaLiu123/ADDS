266064b3405edb00661608613cc099474ea320be
Jingya Liu <88065941+JingyaLiu123@users.noreply.github.com>
2022-05-30 23:13:21 +0930

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,79 @@
-#include "Individual.h"
+#include <vector>
+#include <stack>
+#include <string>
 #include <iostream>
-//using namespace std;
+#include <sstream>
+using namespace std;
 
-int main()
+bool isOperator(char x) {
+  switch (x) {
+  case '+':  case '-':  case '/':  case '*':
+    return true;
+  }
+  return false;
+}
+
+bool OperatorOperandNum(int opratr, int oprnd)
 {
-    Individual t(16);
-    // Individual t("0011001010101000");
-    t.binaryString = "0011001010101000";
-    cout << endl;
-    cout << "getBit: " << t.getBit(11) << endl;
-    cout << "getLength: " << t.getLength() << endl;
-    cout << "getString: " << t.getString() << endl;
-    cout << "flipBit:   ";
-    t.flipBit(10);
-    cout << endl;
+    if (oprnd == opratr + 1) {return true;} else {return false;}
+}
 
-    Individual t1("1001111110011001");
-    cout << endl;
-    cout << "getBit: " << t1.getBit(12) << endl;
-    cout << "getLength: " << t1.getLength() << endl;
-    cout << "getString: " << t1.getString() << endl;
-    cout << "flipBit:   ";
-    t1.flipBit(10);
-    cout << endl;
+bool is0To99(int y)
+{
+    if (0 > y || y > 99){return false;} else {return true;}
+}
 
+string preToInfix(string pre_exp) {
+  stack<string> s;
+  string temp;
+ 
+  int length = pre_exp.size();
+
+  int operator_counter = 0;
+  int operand_counter = 0;
+  string ERR = "ERROR";
+
+  for (int i = length - 1; i >= 0; i--) 
+  {
+
+    if (isOperator(pre_exp[i])) {
+      operator_counter ++;
+
+      string op1 = s.top();   s.pop();
+      string op2 = s.top();   s.pop();
+ 
+      string temp = "(" + op1 + pre_exp[i] + op2 + ")";
+      s.push(temp);
+    }
+    else {
+        if (isdigit(pre_exp[i]) && is0To99(pre_exp[i])) {
+           operand_counter ++;
+           s.push(string(1, pre_exp[i]));
+        } else {
+            s.push(ERR);
+            break;
+        }
+    } 
+  }
+    if (OperatorOperandNum(operator_counter , operand_counter)) {
+        return s.top();
+    } else {
+        s.push(ERR);
+    }
+
+    return s.top();
+}
+
+int main(void)
+{
+    string input;
+    getline(cin, input);
+    istringstream ss(input);
+    string temp_input;
+    string final_input;
+    while(std::getline(ss, temp_input, ' ')) {
+        if(final_input.empty()){final_input = temp_input;} 
+        else {final_input = final_input + temp_input;}
+}
+    cout << preToInfix(final_input);   
 }
